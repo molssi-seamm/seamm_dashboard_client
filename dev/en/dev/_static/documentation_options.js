f67f55b58ca1f15abd0454337d5059b2c5abd6b5
seamm <seamm@users.noreply.github.com>
2024-06-26 22:00:54 +0000

    Updating Docs for commit c9f8e24517a0e5dc692dec7babea4c170974e6bd made on 2024-06-26T21:59:29+00:00 from refs/tags/2024.6.27 by seamm

---
+++ dev/en/dev/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '2024.5.23+1.gb7d19a8',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
